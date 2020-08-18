f6922d76aca33962a4f0400a83d5da0501b9a859
SUYOUNG KIM <suyoung154@gmail.com>
2020-08-18 13:00:26 +0900

    Handling User Input

---
+++ Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by SUYOUNG KIM on 2020/08/18.
+//  Copyright © 2020 SUYOUNG KIM. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
